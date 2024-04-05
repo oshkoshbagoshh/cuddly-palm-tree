872b03c665144593bfc2b8fd3567c2f2e2083dd5
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-04-05 05:40:33 -0500

    initial setup of classes

---
+++ email_campaign_tracker/campaigns/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+# Campaign class
+class Campaign(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+# subscriber class
+class Subscriber(models.Model):
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.email
+    
+
+# Email Class 
+
+# Email Recipient Class
+
