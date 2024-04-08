a5b5e84a03fcd4417a876ae16a7e2a482731dc1b
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-04-07 21:37:09 -0500

    Update base.html and registration-form.html templates

---
+++ app/static/js/allEmails.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: AJ Javadi 
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2024-04-07 20:03:05 
+ * @Last Modified by: AJ Javadi
+ * @Last Modified time: 2024-04-07 20:06:31
+ * @Description: file:///Users/aj/for_benji/cuddly-palm-tree/app/static/js/allEmails.js
+ */
+/*
+ * @Author: AJ Javadi 
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2024-04-07 20:06:29 
+ * @Last Modified by:   undefined 
+ * @Last Modified time: 2024-04-07 20:06:29
+ * @Description: file:///Users/aj/for_benji/cuddly-palm-tree/app/static/js/allEmails.js
+ */
